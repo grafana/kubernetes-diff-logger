20a5fe9b6e0ed815698499d37fb4c092b19ebd8c
Joe Elliott <number101010@gmail.com>
2019-10-22 14:12:33 -0400

    Added namespace to test script
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ go build ../
 
 sleep 5
 
-./kubernetes-diff-logger -kubeconfig=$(k3d get-kubeconfig --name="${K3D_NAME}")
+./kubernetes-diff-logger -kubeconfig=$(k3d get-kubeconfig --name="${K3D_NAME}") -namespace=default
 
 k3d delete --name $K3D_NAME
 rm ./kubernetes-diff-logger
