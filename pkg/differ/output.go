dbbe20ed148d8f7882a3ca2da240b54907d7f3ac
Joe Elliott <number101010@gmail.com>
2019-10-24 15:16:49 -0400

    Added namespace
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/differ/output.go
+++ pkg/differ/output.go
@@ -10,9 +10,9 @@ import (
 
 // Output abstracts a straightforward way to write
 type Output interface {
-	WriteAdded(name string, objectType string)
-	WriteDeleted(name string, objectType string)
-	WriteUpdated(name string, objectType string, diffs []string)
+	WriteAdded(name string, namespace string, objectType string)
+	WriteDeleted(name string, namespace string, objectType string)
+	WriteUpdated(name string, namespace string, objectType string, diffs []string)
 }
 
 // OutputFormat encodes
@@ -37,6 +37,7 @@ type jsonformat struct {
 	ObjectType string `json:"type"`
 	Notes      string `json:"notes"`
 	Name       string `json:"name"`
+	Namespace  string `json:"namespace"`
 }
 
 // NewOutput constructs a new outputter
@@ -48,35 +49,36 @@ func NewOutput(fmt OutputFormat, logAdded bool, logDeleted bool) Output {
 	}
 }
 
-func (f *output) WriteAdded(name string, objectType string) {
+func (f *output) WriteAdded(name string, namespace string, objectType string) {
 	if !f.logAdded {
 		return
 	}
 
-	f.write(name, "added", objectType, nil)
+	f.write(name, namespace, "added", objectType, nil)
 }
 
-func (f *output) WriteDeleted(name string, objectType string) {
+func (f *output) WriteDeleted(name string, namespace string, objectType string) {
 	if !f.logDeleted {
 		return
 	}
 
-	f.write(name, "deleted", objectType, nil)
+	f.write(name, namespace, "deleted", objectType, nil)
 }
 
-func (f *output) WriteUpdated(name string, objectType string, diffs []string) {
-	f.write(name, "updated", objectType, diffs)
+func (f *output) WriteUpdated(name string, namespace string, objectType string, diffs []string) {
+	f.write(name, namespace, "updated", objectType, diffs)
 }
 
-func (f *output) write(name string, verb string, objectType string, etc []string) {
+func (f *output) write(name string, namespace string, verb string, objectType string, etc []string) {
 
 	switch f.format {
 	case Text:
-		fmt.Printf("%s %s : %s (%s) %v\n", time.Now().UTC().Format(time.RFC3339), verb, name, objectType, etc)
+		fmt.Printf("%s %s : %s %s (%s) %v\n", time.Now().UTC().Format(time.RFC3339), verb, namespace, name, objectType, etc)
 	case JSON:
 		bytes, err := json.Marshal(jsonformat{
 			Timestamp:  time.Now().UTC().Format(time.RFC3339),
 			Name:       name,
+			Namespace:  namespace,
 			Verb:       verb,
 			ObjectType: objectType,
 			Notes:      fmt.Sprintf("%v", etc),
